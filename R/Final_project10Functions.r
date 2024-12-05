9e8f1fdf772f8c34a5af4f14fc989583c876fc70
Hayden <161386238+Haydenrb@users.noreply.github.com>
2024-12-05 01:50:35 -0600

    Finished Documentation and test script
    Finished documenting functions. Removed the sample function. Have a test for each function in test script

--- R/Final_project10Functions.r
+++ R/Final_project10Functions.r
@@ -1,82 +1,3 @@
-#' @title Simple Monte-Carlo integration
-#'
-#' @description Compute an approximation of the integral of the function f(x)
-#' with respect to dx in the range [a, b] by Monte-Carlo integration using
-#' uniform sampling.
-#' @param x_range A \code{vector} of dimension 2 used to denote the integration
-#' region of interest, i.e. [a, b].
-#' @param fun A \code{string} containing the function to be integrated. It
-#' is assumed that \code{x} is used as the variable of interest.
-#' @param B A \code{numeric} (integer) used to denote the number of simulations.
-#' @param seed A \code{numeric} used to control the seed of the random number
-#' generator used by this function.
-#' @return A \code{list} containing the following attributes:
-#' \describe{
-#'      \item{I}{Estimated value of the integral}
-#'      \item{var}{Estimated variance of the estimator}
-#' }
-#' @author Stephane Guerrier
-#' @importFrom stats runif
-#' @export
-#' @examples
-#' mc_int(x_range = c(0,1), fun = "x^2", B = 10^5)
-#' mc_int(x_range = c(0,1), fun = "x^2*sin(x^2/pi)", B = 10^5)
-mc_int = function(x_range, fun, B, seed = 1291){
-  # A few checks
-  # Check x_range
-  if (length(x_range) != 2 || x_range[1] >= x_range[2]){
-    stop("x_range is incorrectly specified")
-  }
-
-  # Check fun
-  if (class(fun) != "character"){
-    stop("fun is incorrectly specified and should be a character")
-  }
-
-  x = mean(x_range)
-  test_fun = try(eval(parse(text = fun)), silent = TRUE)
-  if (class(test_fun) == "try-error"){
-    stop("fun cannot be evaluated")
-  }
-
-  # Check B
-  if (B < 1){
-    error("B is incorrectly specified")
-  }
-
-  # Set seed
-  set.seed(seed)
-
-  # Compute the length of the interval, i.e. (b-a)
-  interval_length = diff(x_range)
-
-  # Let's draw some uniforms to get Ui and Xi
-  Ui = runif(B)
-  Xi = x_range[1] + Ui*interval_length
-
-  # Compute \hat{I}
-  x = Xi
-  I_hat = interval_length*mean(eval(parse(text = fun)))
-
-  # Compute \hat{I}_2
-  I2_hat = interval_length*mean((eval(parse(text = fun)))^2)
-  var_I_hat = (interval_length*I2_hat - I_hat^2)/B
-
-  # Output list
-  out = list(I = I_hat, var = var_I_hat,
-             fun = fun, x_range = x_range, B = B)
-  class(out) = "MCI"
-  out
-}
-
-
-# Our functions
-
-# loss function, just following the equations and lectures codes 11_12 and 11_14
-
-
-
-
 
 # Initial beta comes from least-squares
 #' @title Initial Beta Function
@@ -109,7 +30,7 @@ beta_initial <- function(X,y) {
 
 #' @title Loss Function
 #'
-#' @description Calculates the negative log likelihood p_i and plugs it into the loss function
+#' @description Calculates the negative log likelihood p_i and plugs it into the loss function to get calculated loss
 #' @param beta A \code{vector} of coefficients associated with the features in xi.
 #' @param X The \bold{ith} row of the predictors
 #' @param y the \bold{ith} observation of the response
@@ -138,8 +59,9 @@ loss_function <- function(beta, X, y) {
 # Generate predictor variables: first column is normal, second column is Poisson with lambda equal to whatever number the user says
 # Function to generate predictor variables
 #' @title Predictor Variable Generation
-#'
-#' @description Generate the predictor variables for the data
+#' @description This function generates a matrix of predictor variables for use in logistic regression models.
+#' It creates a matrix with an intercept column, a column of normally distributed random variables,
+#' and a column of Poisson-distributed random variables.
 #' @param n An \code{integer} specifying the number of observations
 #' @param lambda The rate parameter for the Poisson distribution
 #' @return Predictor variables \code{matrix} with 2 columns
@@ -215,7 +137,7 @@ fit_model <- function(X, y) {
 # User enters the number of bootstrap samples
 #' @title Bootstrap
 #'
-#' @description Calculates bootstrap confidence intervals
+#' @description Calculates bootstrap confidence intervals for the logistic regression model coefficients.
 #' @param X The design \code{matrix} of independent variables
 #' @param y The \code{vector} of dependent variables (Response variables)
 #' @param B The number of bootstraps. Default is 20
@@ -316,6 +238,41 @@ compute_metrics <- function(X, beta_hat, y) {
   return(metrics)
 }
 
+
+
+
+
+
+
+#' @title Logistic Regression Pipeline
+#'
+#' @description Calculates optimized coefficients, the confidence interval,
+#' and performance metrics for the logistic regression model. Then it returns all of these values in a list
+#' @param n An \code{integer} specifying the number of observations
+#' @param lambda The rate parameter for the Poisson distribution
+#' @param beta_true A \code{vector} of true coefficients
+#' @param B The number of bootstraps. Default is 20
+#' @param alpha The significance level to obtain for the 1 - alpha confidence intervals for Beta. Default is .05
+#' @return A \code{list} of key logistic regression model findings
+#' \describe{
+#'      \item{beta_hat}{The optimized coefficients}
+#'      \item{confidence_interval}{The confidence interval}
+#'      \item{metrics}{The key performance metrics for the logistic regression model as a \code{list}}
+#' }
+#' @author Destanie Pitt, Hayden Billmann, Ashton Wise
+#' @export
+#' @examples
+#' # Example usage:
+#' # Set parameters for logistic regression pipeline
+#' n <- 100
+#' lambda <- 3
+#' beta_true <- c(0.5, -1, 2)
+#' B <- 50
+#' alpha <- 0.05
+#' results <- logistic_regression_pipeline(n, lambda, beta_true, B, alpha)
+#' print(results$beta_hat)
+#' print(results$confidence_interval)
+#' print(results$metrics)
 logistic_regression_pipeline <- function(n, lambda, beta_true, B = 20, alpha = 0.05) {
   # Generate data
   X <- generate_predictors(n, lambda)
--- _Estimater(NotSureWhatToDoWithWhatIsLeftHere)/test.r
+++ _Estimater(NotSureWhatToDoWithWhatIsLeftHere)/test.r
@@ -1,4 +1,6 @@
 library(Estimater)
+
+#beta_initial
 result <- mc_int(x_range = c(0, 1), fun = "x^2", B = 10^5)
 str(result)
 loss_function({1}, {1}, 1)
@@ -7,23 +9,24 @@ y <- c(2, 3, 4, 5)
 beta_initial(X, y)
 
 
-
+#loss_function
 beta <- c(1, 2)
 X <- matrix(c(1, 1, 2, 3), ncol = 2)
 y <- c(1, 0)
 loss_function(beta, X, y)
 
+#generate_predictors
 predictors <- generate_predictors(5, 3)
 print(predictors)
 
 
-
+#generate_response
 beta_true <- c(0.5, -1, 2)
 X <- matrix(c(1, 2, 3, 4, 5, 6), ncol = 3)
 response <- generate_response(X, beta_true)
 print(response)
 
-
+#fit_model
 X <- matrix(c(1, 1, 1, 1, 2, 3, 4, 5), ncol = 2)
 y <- c(2, 3, 4, 5)
 beta_initial(X, y)
@@ -32,14 +35,26 @@ print(optimized_beta)
 
 
 
-
+#bootstrap
 X <- matrix(c(1, 1, 1, 1, 2, 3, 4, 5), ncol = 2)
 y <- c(2, 3, 4, 5)
 print(bootstrap(X, y, 20, .05))
 
 
-
+#compute_metrics
 X <- matrix(c(1, 1, 1, 1, 2, 3, 4, 5), ncol = 2)
 y <- c(2, 3, 4, 5)
 b <- beta_initial(X,y)
 print(compute_metrics(X,b,y))
+
+
+#logistic_regression_pipeline
+n <- 100
+lambda <- 3
+beta_true <- c(0.5, -1, 2)
+B <- 50
+alpha <- 0.05
+results <- logistic_regression_pipeline(n, lambda, beta_true, B, alpha)
+print(results$beta_hat)
+print(results$confidence_interval)
+print(results$metrics)
